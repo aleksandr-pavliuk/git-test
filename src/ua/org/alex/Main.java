b4e4268e7b0a01046bf4663a61ba117d338ac7ea
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-05-19 15:17:27 +0300

    Revert "Hello java"
    This reverts commit dbe15a80242fefffdc812e45e7b4ba6cd1de8111.

--- src/ua/org/alex/Main.java
+++ src/ua/org/alex/Main.java
@@ -3,6 +3,6 @@ package ua.org.alex;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello java");
+        System.out.println("Hello world");
     }
 }
